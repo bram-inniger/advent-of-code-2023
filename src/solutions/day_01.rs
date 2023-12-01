aedf13447476c91d9b3598d63a9904a8d9ce30d8
Bram Inniger <bram@inniger.be>
2023-12-02 01:36:07 +0200

    Removed runtime reading of files to compile-time. Added benefit that now the constant "&str <-> String" wrangling is almost gone and the code is simplified.

--- src/solutions/day_01.rs
+++ src/solutions/day_01.rs
@@ -1,35 +1,30 @@
 use regex::Regex;
 use std::str::FromStr;
 
-pub fn solve_1(document: Vec<String>) -> u32 {
+pub fn solve_1(document: Vec<&str>) -> u32 {
     let re = Regex::new(r"([1-9])").unwrap();
 
     solve(&document, &re, &re)
 }
 
-pub fn solve_2(document: Vec<String>) -> u32 {
+pub fn solve_2(document: Vec<&str>) -> u32 {
     let re = Regex::new(r"([1-9]|one|two|three|four|five|six|seven|eight|nine)").unwrap();
     let re_rev = Regex::new(r"([1-9]|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno)").unwrap();
 
     solve(&document, &re, &re_rev)
 }
 
-fn solve(document: &Vec<String>, re: &Regex, re_rev: &Regex) -> u32 {
-    let mut sum = 0;
+fn solve(document: &[&str], re: &Regex, re_rev: &Regex) -> u32 {
+    document
+        .iter()
+        .map(|&line| {
+            let first = re.find(line).unwrap().as_str();
+            let last = &reverse(re_rev.find(&reverse(line)).unwrap().as_str());
 
-    for line in document {
-        let line_rev = reverse(line.as_str());
-
-        let first = re.find(line.as_str()).unwrap().as_str();
-        let string = reverse(re_rev.find(line_rev.as_str()).unwrap().as_str());
-        let last = string.as_str();
-
-        let calibration = Calibration::new(first, last);
-
-        sum += calibration.number;
-    }
-
-    sum
+            Calibration::new(first, last)
+        })
+        .map(|calibration| calibration.number)
+        .sum()
 }
 
 fn reverse(string: &str) -> String {
@@ -65,28 +60,24 @@ impl Calibration {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::util;
 
     #[test]
     fn day_01_part_01_sample() {
-        let sample = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
-            .iter()
-            .map(|&s| s.to_string())
-            .collect();
+        let sample = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
 
         assert_eq!(142, solve_1(sample))
     }
 
     #[test]
     fn day_01_part_01_solution() {
-        let input = util::read_inputs(1);
+        let input = include_str!("../../inputs/day_01.txt").lines().collect();
 
         assert_eq!(54_331, solve_1(input))
     }
 
     #[test]
     fn day_01_part_02_sample() {
-        let sample = [
+        let sample = vec![
             "two1nine",
             "eightwothree",
             "abcone2threexyz",
@@ -94,17 +85,14 @@ mod tests {
             "4nineeightseven2",
             "zoneight234",
             "7pqrstsixteen",
-        ]
-        .iter()
-        .map(|&s| s.to_string())
-        .collect();
+        ];
 
         assert_eq!(281, solve_2(sample))
     }
 
     #[test]
     fn day_01_part_02_solution() {
-        let input = util::read_inputs(1);
+        let input = include_str!("../../inputs/day_01.txt").lines().collect();
 
         assert_eq!(54_518, solve_2(input))
     }
