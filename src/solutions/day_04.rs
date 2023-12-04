0981eb3775e51f2eb8b8f0c4b31bc5cb6384b840
Bram Inniger <bram@inniger.be>
2023-12-04 15:20:02 +0200

    Solved D04P02

--- src/solutions/day_04.rs
+++ src/solutions/day_04.rs
@@ -1,23 +1,36 @@
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
 
 pub fn solve_1(cards: Vec<&str>) -> u32 {
+    parse_cards(&cards).iter().map(|c| c.score()).sum()
+}
+
+pub fn solve_2(cards: Vec<&str>) -> u32 {
+    let cards = parse_cards(&cards);
+    let mut pile: HashMap<u32, u32> = cards.iter().map(|c| (c.card_id, 1)).collect();
+
+    for card in cards {
+        let nr_matches = card.nr_matches();
+        let nr_copies = pile[&card.card_id];
+
+        for card_id in (&card.card_id + 1)..(&card.card_id + 1 + nr_matches) {
+            *pile.get_mut(&card_id).unwrap() += nr_copies;
+        }
+    }
+
+    pile.values().sum()
+}
+
+fn parse_cards(cards: &[&str]) -> Vec<Card> {
     let re_card =
         Regex::new(r"^Card\s+(?<card_id>\d+):(?<winning>(?:\s+\d+)+) \|(?<having>(?:\s+\d+)+)$")
             .unwrap();
     let re_numbers = Regex::new(r"\d+").unwrap();
 
-    parse_cards(cards, &re_card, &re_numbers)
-        .iter()
-        .map(|c| c.score())
-        .sum()
-}
-
-fn parse_cards(cards: Vec<&str>, re_card: &Regex, re_numbers: &Regex) -> Vec<Card> {
     cards
         .iter()
-        .map(|card| parse_card(card, re_card, re_numbers))
+        .map(|card| parse_card(card, &re_card, &re_numbers))
         .collect()
 }
 
@@ -26,7 +39,7 @@ fn parse_card(card: &str, re_card: &Regex, re_numbers: &Regex) -> Card {
         panic!("Could not parse: {}", card);
     };
 
-    let _card_id = u32::from_str(&caps["card_id"]).unwrap();
+    let card_id = u32::from_str(&caps["card_id"]).unwrap();
     let winning: HashSet<u32> = re_numbers
         .find_iter(&caps["winning"])
         .map(|m| m.as_str())
@@ -41,31 +54,32 @@ fn parse_card(card: &str, re_card: &Regex, re_numbers: &Regex) -> Card {
         .collect();
 
     Card {
-        _card_id,
+        card_id,
         winning,
         having,
     }
 }
 
 struct Card {
-    _card_id: u32,
+    card_id: u32,
     winning: HashSet<u32>,
     having: Vec<u32>,
 }
 
 impl Card {
     fn score(&self) -> u32 {
-        let matches = self
-            .having
-            .iter()
-            .filter(|n| self.winning.contains(n))
-            .count() as u32;
-
-        if matches > 0 {
-            2_u32.pow(matches - 1)
+        if Self::nr_matches(self) > 0 {
+            2_u32.pow(Self::nr_matches(self) - 1)
         } else {
             0
         }
+    }
+
+    fn nr_matches(&self) -> u32 {
+        self.having
+            .iter()
+            .filter(|n| self.winning.contains(n))
+            .count() as u32
     }
 }
 
@@ -92,5 +106,26 @@ mod tests {
         let input = include_str!("../../inputs/day_04.txt").lines().collect();
 
         assert_eq!(23_673, solve_1(input));
+    }
+
+    #[test]
+    fn day_04_part_02_sample() {
+        let sample = vec![
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        ];
+
+        assert_eq!(30, solve_2(sample));
+    }
+
+    #[test]
+    fn day_04_part_02_solution() {
+        let input = include_str!("../../inputs/day_04.txt").lines().collect();
+
+        assert_eq!(12_263_631, solve_2(input));
     }
 }
