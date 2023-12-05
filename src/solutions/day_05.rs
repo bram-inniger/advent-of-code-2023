b094240e00b1856cd9482baec53e8be251c49eb3
Bram Inniger <bram@inniger.be>
2023-12-05 18:56:56 +0200

    Solved D05P02

--- src/solutions/day_05.rs
+++ src/solutions/day_05.rs
@@ -12,6 +12,17 @@ pub fn solve_1(almanac: &str) -> u64 {
         .unwrap()
 }
 
+pub fn solve_2(almanac: &str) -> u64 {
+    let almanac = Almanac::new(almanac);
+
+    almanac
+        .seeds_as_ranges()
+        .iter()
+        .map(|e| almanac.map_to(e))
+        .min()
+        .unwrap()
+}
+
 struct Almanac {
     seeds: Vec<u64>,
     categories: Vec<Category>,
@@ -49,6 +60,20 @@ impl Almanac {
         }
 
         location
+    }
+
+    fn seeds_as_ranges(&self) -> Vec<u64> {
+        let mut seeds = Vec::new();
+
+        let iter = &mut self.seeds.iter().peekable();
+        while iter.peek().is_some() {
+            let seed = *iter.next().unwrap();
+            let range = *iter.next().unwrap();
+
+            (seed..seed + range).for_each(|s| seeds.push(s));
+        }
+
+        seeds
     }
 }
 
@@ -142,5 +167,52 @@ mod tests {
         let input = include_str!("../../inputs/day_05.txt");
 
         assert_eq!(199_602_917, solve_1(input));
+    }
+
+    #[test]
+    fn day_05_part_02_sample() {
+        let sample = "seeds: 79 14 55 13\n\
+            \n\
+            seed-to-soil map:\n\
+            50 98 2\n\
+            52 50 48\n\
+            \n\
+            soil-to-fertilizer map:\n\
+            0 15 37\n\
+            37 52 2\n\
+            39 0 15\n\
+            \n\
+            fertilizer-to-water map:\n\
+            49 53 8\n\
+            0 11 42\n\
+            42 0 7\n\
+            57 7 4\n\
+            \n\
+            water-to-light map:\n\
+            88 18 7\n\
+            18 25 70\n\
+            \n\
+            light-to-temperature map:\n\
+            45 77 23\n\
+            81 45 19\n\
+            68 64 13\n\
+            \n\
+            temperature-to-humidity map:\n\
+            0 69 1\n\
+            1 0 69\n\
+            \n\
+            humidity-to-location map:\n\
+            60 56 37\n\
+            56 93 4";
+
+        assert_eq!(46, solve_2(sample));
+    }
+
+    #[test]
+    #[ignore = "very slow due to a brute-force approach"]
+    fn day_05_part_02_solution() {
+        let input = include_str!("../../inputs/day_05.txt");
+
+        assert_eq!(2_254_686, solve_2(input));
     }
 }
