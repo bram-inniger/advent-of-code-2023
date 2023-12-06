13847405bc028b66101a14f00ffbc3e626af5ad2
Bram Inniger <bram@inniger.be>
2023-12-06 10:55:06 +0200

    Solved D06P02

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,22 +1,30 @@
 use regex::Regex;
 use std::str::FromStr;
 
-pub fn solve_1(races: Vec<&str>) -> u32 {
-    parse_races(races).iter().map(|r| r.nr_wins()).product()
+pub fn solve_1(races: Vec<&str>) -> u64 {
+    solve(parse_races_bad_kerning(races))
 }
 
-fn parse_races(timings: Vec<&str>) -> Vec<Race> {
+pub fn solve_2(races: Vec<&str>) -> u64 {
+    solve(parse_races_good_kerning(races))
+}
+
+fn solve(races: Vec<Race>) -> u64 {
+    races.iter().map(|r| r.nr_wins()).product()
+}
+
+fn parse_races_bad_kerning(timings: Vec<&str>) -> Vec<Race> {
     let re = Regex::new(r"\d+").unwrap();
 
-    let times: Vec<u32> = re
+    let times: Vec<u64> = re
         .find_iter(timings[0])
         .map(|m| m.as_str())
-        .flat_map(u32::from_str)
+        .flat_map(u64::from_str)
         .collect();
-    let distances: Vec<u32> = re
+    let distances: Vec<u64> = re
         .find_iter(timings[1])
         .map(|m| m.as_str())
-        .flat_map(u32::from_str)
+        .flat_map(u64::from_str)
         .collect();
 
     times
@@ -26,18 +34,30 @@ fn parse_races(timings: Vec<&str>) -> Vec<Race> {
         .collect()
 }
 
+fn parse_races_good_kerning(timings: Vec<&str>) -> Vec<Race> {
+    let re = Regex::new(r"\d+").unwrap();
+
+    let time: String = re.find_iter(timings[0]).map(|m| m.as_str()).collect();
+    let distance: String = re.find_iter(timings[1]).map(|m| m.as_str()).collect();
+
+    vec![Race {
+        time: u64::from_str(&time).unwrap(),
+        distance: u64::from_str(&distance).unwrap(),
+    }]
+}
+
 #[derive(Debug)]
 struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
 impl Race {
-    fn nr_wins(&self) -> u32 {
+    fn nr_wins(&self) -> u64 {
         (1..self.time)
             .map(|speed| speed * (self.time - speed))
             .filter(|&distance| distance > self.distance)
-            .count() as u32
+            .count() as u64
     }
 }
 
@@ -57,5 +77,19 @@ mod tests {
         let input = include_str!("../../inputs/day_06.txt").lines().collect();
 
         assert_eq!(160_816, solve_1(input));
+    }
+
+    #[test]
+    fn day_06_part_02_sample() {
+        let sample = vec!["Time:      7  15   30", "Distance:  9  40  200"];
+
+        assert_eq!(71_503, solve_2(sample));
+    }
+
+    #[test]
+    fn day_06_part_02_solution() {
+        let input = include_str!("../../inputs/day_06.txt").lines().collect();
+
+        assert_eq!(46_561_107, solve_2(input));
     }
 }
