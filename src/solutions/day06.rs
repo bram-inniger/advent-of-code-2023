db4f25f9809fb89978b8de6b87393b367de7715c
Bram Inniger <bram@inniger.be>
2023-12-06 11:39:54 +0200

    Refactored D06P02 to use a closed form solution

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -2,15 +2,17 @@ use regex::Regex;
 use std::str::FromStr;
 
 pub fn solve_1(races: Vec<&str>) -> u64 {
-    solve(parse_races_bad_kerning(races))
+    parse_races_bad_kerning(races)
+        .iter()
+        .map(|r| r.nr_wins())
+        .product()
 }
 
 pub fn solve_2(races: Vec<&str>) -> u64 {
-    solve(parse_races_good_kerning(races))
-}
-
-fn solve(races: Vec<Race>) -> u64 {
-    races.iter().map(|r| r.nr_wins()).product()
+    parse_races_good_kerning(races)
+        .iter()
+        .map(|r| r.nr_wins_closed_form())
+        .product()
 }
 
 fn parse_races_bad_kerning(timings: Vec<&str>) -> Vec<Race> {
@@ -58,6 +60,36 @@ impl Race {
             .map(|speed| speed * (self.time - speed))
             .filter(|&distance| distance > self.distance)
             .count() as u64
+    }
+
+    // The problem can be solved using a quadratic equation
+    // The unknown (x) is how long to press
+    // Knowns are the time available (time), and the distance to beat (dist)
+    //
+    // This creates the following equation, which we can simplify:
+    // => x * (time - x) > dist
+    // => -x^2 + x * time - dist > 0
+    //
+    // This gives us the following parameters to solve for the roots
+    // => a = -1
+    // => b = time
+    // => c = -dist
+    //
+    // Finally the roots can be found at
+    // => (-b +- sqrt(b * b - 4 * a * c)) / 2 * a
+    //
+    // The solution then becomes counting the number of elements in the range formed by the roots
+    fn nr_wins_closed_form(&self) -> u64 {
+        let a = -1.0;
+        let b = self.time as f64;
+        let c = -(self.distance as f64);
+
+        let d = b * b - 4.0 * a * c;
+
+        let root_1 = (-b + d.sqrt()) / -2.0;
+        let root_2 = (-b - d.sqrt()) / -2.0;
+
+        (root_2 - root_1) as u64
     }
 }
 
