109251b41d7953d0d4f58a005ebfd0158684d68f
Bram Inniger <bram@inniger.be>
2023-12-12 10:55:14 +0200

    Preparing for D10P02, added tests

--- src/solutions/day_10.rs
+++ src/solutions/day_10.rs
@@ -5,6 +5,10 @@ pub fn solve_1(maze: Vec<&str>) -> usize {
     Maze::new(maze).pipe_loop.len() / 2
 }
 
+pub fn solve_2(_maze: Vec<&str>) -> usize {
+    42
+}
+
 #[derive(Debug)]
 struct Maze {
     tiles: Vec<Vec<Tile>>,
@@ -168,5 +172,75 @@ mod tests {
         let input = include_str!("../../inputs/day_10.txt").lines().collect();
 
         assert_eq!(7_107, solve_1(input));
+    }
+
+    #[ignore]
+    #[test]
+    fn day_10_part_02_sample() {
+        let sample = vec![
+            "...........",
+            ".S-------7.",
+            ".|F-----7|.",
+            ".||.....||.",
+            ".||.....||.",
+            ".|L-7.F-J|.",
+            ".|..|.|..|.",
+            ".L--J.L--J.",
+            "...........",
+        ];
+
+        assert_eq!(4, solve_2(sample));
+
+        let sample = vec![
+            "..........",
+            ".S------7.",
+            ".|F----7|.",
+            ".||....||.",
+            ".||....||.",
+            ".|L-7F-J|.",
+            ".|..||..|.",
+            ".L--JL--J.",
+            "..........",
+        ];
+
+        assert_eq!(4, solve_2(sample));
+
+        let sample = vec![
+            ".F----7F7F7F7F-7....",
+            ".|F--7||||||||FJ....",
+            ".||.FJ||||||||L7....",
+            "FJL7L7LJLJ||LJ.L-7..",
+            "L--J.L7...LJS7F-7L7.",
+            "....F-J..F7FJ|L7L7L7",
+            "....L7.F7||L7|.L7L7|",
+            ".....|FJLJ|FJ|F7|.LJ",
+            "....FJL-7.||.||||...",
+            "....L---J.LJ.LJLJ...",
+        ];
+
+        assert_eq!(8, solve_2(sample));
+
+        let sample = vec![
+            "FF7FSF7F7F7F7F7F---7",
+            "L|LJ||||||||||||F--J",
+            "FL-7LJLJ||||||LJL-77",
+            "F--JF--7||LJLJ7F7FJ-",
+            "L---JF-JLJ.||-FJLJJ7",
+            "|F|F-JF---7F7-L7L|7|",
+            "|FFJF7L7F-JF7|JL---7",
+            "7-L-JL7||F7|L7F-7F7|",
+            "L.L7LFJ|||||FJL7||LJ",
+            "L7JLJL-JLJLJL--JLJ.L",
+        ];
+
+        assert_eq!(10, solve_2(sample));
+    }
+
+    #[ignore]
+    #[test]
+    fn day_10_part_02_solution() {
+        let input = include_str!("../../inputs/day_10.txt").lines().collect();
+
+        assert_eq!(0, solve_2(input));
     }
 }
