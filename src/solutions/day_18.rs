613ba7b7ff904aa9454711c475562fea436c740a
Bram Inniger <bram@inniger.be>
2023-12-18 20:10:15 +0200

    Solved D18P02

--- src/solutions/day_18.rs
+++ src/solutions/day_18.rs
@@ -2,8 +2,14 @@ use itertools::Itertools;
 use regex::Regex;
 use std::str::FromStr;
 
-pub fn solve_1(plan: Vec<&str>) -> u32 {
-    Terrain::new(plan).dig_lagoon()
+const BASE_16: u32 = 16;
+
+pub fn solve_1(plan: Vec<&str>) -> u64 {
+    Terrain::new(plan, false).dig_lagoon()
+}
+
+pub fn solve_2(plan: Vec<&str>) -> u64 {
+    Terrain::new(plan, true).dig_lagoon()
 }
 
 #[derive(Debug)]
@@ -12,7 +18,7 @@ struct Terrain {
 }
 
 impl Terrain {
-    fn new(plan: Vec<&str>) -> Terrain {
+    fn new(plan: Vec<&str>, swapped: bool) -> Terrain {
         let re = Regex::new(r"^(?<direction>[URDL]) (?<steps>\d+) \(#(?<colour>.+)\)$").unwrap();
 
         let instructions = plan
@@ -20,32 +26,37 @@ impl Terrain {
             .map(|&hay| {
                 let caps = re.captures(hay).unwrap();
 
-                let direction = match &caps["direction"] {
-                    "U" => Direction::Up,
-                    "R" => Direction::Right,
-                    "D" => Direction::Down,
-                    "L" => Direction::Left,
-                    _ => unreachable!(),
+                let direction = if swapped {
+                    match &caps["colour"][5..] {
+                        "0" => Direction::Right,
+                        "1" => Direction::Down,
+                        "2" => Direction::Left,
+                        "3" => Direction::Up,
+                        _ => unreachable!(),
+                    }
+                } else {
+                    match &caps["direction"] {
+                        "R" => Direction::Right,
+                        "D" => Direction::Down,
+                        "L" => Direction::Left,
+                        "U" => Direction::Up,
+                        _ => unreachable!(),
+                    }
                 };
-                let steps = u32::from_str(&caps["steps"]).unwrap();
-                let _colour = Colour {
-                    _red: u8::from_str_radix(&caps["colour"][..2], 16).unwrap(),
-                    _green: u8::from_str_radix(&caps["colour"][2..4], 16).unwrap(),
-                    _blue: u8::from_str_radix(&caps["colour"][4..], 16).unwrap(),
+                let steps = if swapped {
+                    u32::from_str_radix(&caps["colour"][..5], BASE_16).unwrap()
+                } else {
+                    u32::from_str(&caps["steps"]).unwrap()
                 };
 
-                Instruction {
-                    direction,
-                    steps,
-                    _colour,
-                }
+                Instruction { direction, steps }
             })
             .collect_vec();
 
         Terrain { instructions }
     }
 
-    fn dig_lagoon(&self) -> u32 {
+    fn dig_lagoon(&self) -> u64 {
         let mut current = (0, 0);
         let mut perimeter = 0;
         let mut coords = Vec::new();
@@ -54,13 +65,13 @@ impl Terrain {
 
         for instruction in &self.instructions {
             match instruction.direction {
-                Direction::Up => current.1 -= instruction.steps as i32,
-                Direction::Right => current.0 += instruction.steps as i32,
-                Direction::Down => current.1 += instruction.steps as i32,
-                Direction::Left => current.0 -= instruction.steps as i32,
+                Direction::Up => current.1 -= instruction.steps as i64,
+                Direction::Right => current.0 += instruction.steps as i64,
+                Direction::Down => current.1 += instruction.steps as i64,
+                Direction::Left => current.0 -= instruction.steps as i64,
             }
 
-            perimeter += instruction.steps;
+            perimeter += instruction.steps as u64;
             coords.push(current);
         }
 
@@ -83,22 +94,14 @@ impl Terrain {
 struct Instruction {
     direction: Direction,
     steps: u32,
-    _colour: Colour,
 }
 
 #[derive(Debug)]
 enum Direction {
-    Up,
     Right,
     Down,
     Left,
-}
-
-#[derive(Debug)]
-struct Colour {
-    _red: u8,
-    _green: u8,
-    _blue: u8,
+    Up,
 }
 
 #[cfg(test)]
@@ -132,5 +135,34 @@ mod tests {
         let input = include_str!("../../inputs/day_18.txt").lines().collect();
 
         assert_eq!(72_821, solve_1(input));
+    }
+
+    #[test]
+    fn day_18_part_02_sample() {
+        let sample = vec![
+            "R 6 (#70c710)",
+            "D 5 (#0dc571)",
+            "L 2 (#5713f0)",
+            "D 2 (#d2c081)",
+            "R 2 (#59c680)",
+            "D 2 (#411b91)",
+            "L 5 (#8ceee2)",
+            "U 2 (#caa173)",
+            "L 1 (#1b58a2)",
+            "U 2 (#caa171)",
+            "R 2 (#7807d2)",
+            "U 3 (#a77fa3)",
+            "L 2 (#015232)",
+            "U 2 (#7a21e3)",
+        ];
+
+        assert_eq!(952_408_144_115, solve_2(sample));
+    }
+
+    #[test]
+    fn day_18_part_02_solution() {
+        let input = include_str!("../../inputs/day_18.txt").lines().collect();
+
+        assert_eq!(127_844_509_405_501, solve_2(input));
     }
 }
