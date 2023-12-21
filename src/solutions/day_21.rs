2440b170cc247af4226e1f0bb1c5fa4da7c8852f
Bram Inniger <bram@inniger.be>
2023-12-21 22:29:58 +0200

    Solved D21P02

--- src/solutions/day_21.rs
+++ src/solutions/day_21.rs
@@ -3,22 +3,61 @@ use std::ops::Not;
 
 use rustc_hash::{FxHashMap, FxHashSet};
 
-pub fn solve_1(garden: &[&str], steps: u8) -> usize {
+pub fn solve_1(garden: &[&str], steps: u8) -> u16 {
     Garden::new(garden)
         .reachable(steps)
         .iter()
         .filter(|(_, &d)| d % 2 == 0)
-        .count()
+        .count() as u16
+}
+
+pub fn solve_2(garden: &[&str], steps: u64) -> u64 {
+    let garden = Garden::new(garden);
+    let visited = garden.reachable(u8::MAX);
+
+    // Solution based on https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21
+    let even_all = visited.values().filter(|&&v| v % 2 == 1).count() as u64;
+    let odd_all = visited.values().filter(|&&v| v % 2 == 0).count() as u64;
+    let even_corners = visited
+        .values()
+        .filter(|&&v| v % 2 == 0 && v > 65)
+        .count() as u64;
+    let odd_corners = visited
+        .values()
+        .filter(|&&v| v % 2 == 1 && v > 65)
+        .count() as u64;
+
+    // The input number "26_501_365" isn't randomly chosen,
+    // it decomposes as "65 + (202300 * 131)".
+    // "65" is the nr steps needed to walk to the edge of one garden square
+    // "131" is the width of 1 garden square.
+    // This means per direction the max distance we can walk is "202300" full additional squares.
+    //
+    // This same variable "n" will hold that value "202300".
+    //
+    // These arrange neatly in a "diamond" (like the card suit).
+    // Counting all reachable steps taking into account odd and even distances,
+    // and taking care of the edge condition at the corners yields the result.
+    let n = (steps - (garden.init_w as u64 / 2)) / garden.init_h as u64;
+
+    (n + 1) * (n + 1) * even_all
+        + (n * n) * odd_all
+        - (n + 1) * odd_corners
+        + n * even_corners
 }
 
 #[derive(Debug)]
 struct Garden {
     plots: FxHashSet<(i16, i16)>,
     start: (i16, i16),
+    init_h: i16,
+    init_w: i16,
 }
 
 impl Garden {
     fn new(plots: &[&str]) -> Garden {
+        let init_h = plots.len() as i16;
+        let init_w = plots[0].len() as i16;
         let plots: FxHashMap<_, _> = plots
             .iter()
             .enumerate()
@@ -33,7 +72,7 @@ impl Garden {
         let start = *plots.iter().find(|(_, &c)| c == 'S').unwrap().0;
         let plots = plots.into_keys().collect();
 
-        Garden { plots, start }
+        Garden { plots, start, init_h, init_w }
     }
 
     fn reachable(&self, steps: u8) -> FxHashMap<(i16, i16), u8> {
@@ -97,5 +136,44 @@ mod tests {
             .collect_vec();
 
         assert_eq!(3_795, solve_1(&input, 64));
+    }
+
+    #[test]
+    fn day_21_part_02_sample() {
+        // No valid sample input(s) for part 2,
+        // as the real input has a property (open row and column around "start"),
+        // which the sample doesn't have, meaning we cannot run them.
+        // Manually testing them against the code for Part 1 works fine.
+
+        // let sample = vec![
+        //     "...........",
+        //     ".....###.#.",
+        //     ".###.##..#.",
+        //     "..#.#...#..",
+        //     "....#.#....",
+        //     ".##..S####.",
+        //     ".##..#...#.",
+        //     ".......##..",
+        //     ".##.#.####.",
+        //     ".##..##.##.",
+        //     "...........",
+        // ];
+        //
+        // assert_eq!(16, solve_2(&sample, 6));
+        // assert_eq!(50, solve_2(&sample, 10));
+        // assert_eq!(1_594, solve_2(&sample, 50));
+        // assert_eq!(6_536, solve_2(&sample, 100));
+        // assert_eq!(167_004, solve_2(&sample, 500));
+        // assert_eq!(668_697, solve_2(&sample, 1_000));
+        // assert_eq!(16_733_044, solve_2(&sample, 5_000));
+    }
+
+    #[test]
+    fn day_21_part_02_solution() {
+        let input = include_str!("../../inputs/day_21.txt")
+            .lines()
+            .collect_vec();
+
+        assert_eq!(630_129_824_772_393, solve_2(&input, 26_501_365));
     }
 }
